20c6f2d8a74353643711d98bb9713efea2730180
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-07-26 00:15:21 +0200

    done transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from "./Profile/Profile"
 import Statistics from "./Statistics/Statistics"
 import FriendList from "./FriendList/FriendList"
+import TransactionHistory from "./TransactionHistory/TransactionHistory"
 
 import user from 'components/Profile/user.json'
 import stats from 'components/Statistics/data.json'
 import friends from 'components/FriendList/friends.json'
+import transactions from "components/TransactionHistory/transactions.json";
 
 
 export const App = () => {
@@ -15,7 +17,8 @@ export const App = () => {
   tag={user.tag} location={user.location}
   avatar={user.avatar} stats={user.stats} />
   <Statistics title="Upload stats" stats={stats} />
-  <FriendList friends={friends} />;
+  <FriendList friends={friends} />
+  <TransactionHistory items={transactions} />;
   </div>
   )
 };
